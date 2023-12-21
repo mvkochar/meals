0f1a255a6e2340fe8a7e868441f2f42f427c0af1
sgkochar <sgkochar@gmail.com>
2023-12-21 14:31:46 +0200

    add Benefits and Nutrition blocks to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -84,6 +84,88 @@ const Home = () => {
         </div>
         <div><img src="images/about.png" alt="about" /></div>
       </div>
+      <div className="home-benefits d-f">
+        <div><img src="images/benefits.png" alt="benefits" /></div>
+        <div>
+          <h4 className="home-benefits-above">Our Benefits</h4>
+          <h3 className="home-benefits-title">Why choose us?</h3>
+          <p className="home-benefits-desc">
+            We are proud to provide a healthy and nutritious meal for those in need! We listen to your preferences and
+            customize the weekly menu considering your requests! Your health is our priority!
+          </p>
+          <div className="home-benefits-box d-f">
+            <div className="home-benefits-item d-f align-center">
+              <div><img src="images/hamburger.svg" alt="hamburger" /></div>
+              <div className="benefits-item-title">Delicious & Healthy</div>
+            </div>
+            <div className="home-benefits-item d-f align-center">
+              <div><img src="images/heat.svg" alt="heat" /></div>
+              <div className="benefits-item-title">Ready to heat</div>
+            </div>
+            <div className="home-benefits-item d-f align-center">
+              <div><img src="images/truck.svg" alt="truck" /></div>
+              <div className="benefits-item-title">Delivery</div>
+            </div>
+            <div className="home-benefits-item d-f align-center">
+              <div><img src="images/taste.svg" alt="taste" /></div>
+              <div className="benefits-item-title">Taste and preferences</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-nutrition">
+        <div className="home-nutrition-mask"><img src="images/nutrition-mask.png" alt="nutrition-mask" /></div>
+        <h4 className="home-nutrition-above">Nutrition plans</h4>
+        <h3 className="home-nutrition-title">Choose Your Nutrition</h3>
+        <div className="home-nutrition-content">
+          <div className="home-nutrition-features d-f jc-sb">
+            <div className="nutrition-features-item">
+              <div><img src="images/nutrition1.png" alt="nutrition1" /></div>
+              <h5>Gourmet</h5>
+            </div>
+            <div className="nutrition-features-item">
+              <div><img src="images/nutrition2.png" alt="nutrition2" /></div>
+              <h5>Comfort</h5>
+            </div>
+            <div className="nutrition-features-item">
+              <div><img src="images/nutrition3.png" alt="nutrition3" /></div>
+              <h5>Deli</h5>
+            </div>
+            <div className="nutrition-features-item">
+              <div><img src="images/nutrition1.png" alt="nutrition1" /></div>
+              <h5>International</h5>
+            </div>
+            <div className="nutrition-features-item">
+              <div><img src="images/nutrition1.png" alt="nutrition1" /></div>
+              <h5>Family</h5>
+            </div>
+            <div className="nutrition-features-item">
+              <div><img src="images/nutrition1.png" alt="nutrition1" /></div>
+              <h5>Homestyle</h5>
+            </div>
+          </div>
+          <div className="home-nutrition-box d-f">
+            <div><img src="images/meals.png" alt="meals" /></div>
+            <div>
+              <div className="meals-title">Meals includes:</div>
+              <ul className="meals-list">
+                <li className="meals-list-item">Loaf of Bread </li>
+                <li className="meals-list-item">Sticks of Butter</li>
+                <li className="meals-list-item">Quart of Juice</li>
+                <li className="meals-list-item">Half Callon of 2% Milk</li>
+                <li className="meals-list-item">Box of Cereal or Oat Meal</li>
+                <li className="meals-list-item">Package of Cheese Sticks or Cheese Slices</li>
+                <li className="meals-list-item">Pack of Fruits Cups Complimentary Dessert</li>   
+              </ul>
+              <div className="meals-actions d-f">
+                <a href="" className="meals-actions-download">Download menu</a>
+                <button className='meals-actions-started'>Get started</button>
+              </div> 
+            </div>
+          </div>
+
+        </div>
+      </div>
     </main>
   )
 }
