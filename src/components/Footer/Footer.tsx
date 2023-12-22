7f1f4e3e98ffe90116c7fcb62fca3ddacb027f23
sgkochar <sgkochar@gmail.com>
2023-12-22 14:44:44 +0200

    create Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <div className="footer-bar d-f jc-sb">
+        <div>
+          <div className="footer-logo"><img src="images/footer-logo.svg" alt="footer-logo" /></div>
+          <p className="footer-desc">
+            All rights resereved <a href="">MealsAmericaFL.com</a> is based in the Florida area
+            and provides home-delivered meals to consumers across ALL sixty seven counties in Florida.
+          </p>
+        </div>
+        <div>
+          <h4 className="footer-bl-title">Contact info</h4>
+          <div className="footer-contacts d-f">
+            <p className="footer-contacts-address">
+              1733 Benbow Ct, Ste 3, Apopka, FL, 32703
+            </p>
+            <p className="footer-contacts-phone">(786) 600-59-09</p>
+            <p className="footer-contacts-email">Support@MealsAmericaFL.com</p>
+          </div>
+        </div>
+        <div>
+          <h4 className="footer-bl-title">Links</h4>
+          <ul className='footer-bl-list'>
+            <li><a href="">About Us</a></li>
+            <li><a href="">How It Works</a></li>
+            <li><a href="">Our menu</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-rights">&copy; All rights resereved <a href="">MealsAmericaFL.com</a></div>
+    </footer>
   )
 }
 
